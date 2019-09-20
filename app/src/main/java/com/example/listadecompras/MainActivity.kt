d00241728b30009d3a6d74216cb3b539d11d4795
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-20 18:45:22 -0300

    Criação do Adapter de Strings para armazenar os itens da lista

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.listadecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //criado o adapter para exibir os dados na lista
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        //definindo o adaptador na lista
+        lv_lista.adapter = produtosAdapter
+
+
+        
     }
 }
