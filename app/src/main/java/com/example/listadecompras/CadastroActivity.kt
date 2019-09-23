1c3dbd4b1da11baf6b2ea2236afd1be4d0e0257d
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-23 11:24:48 -0300

    Verificação dos campos, se for nulo exibir msg's

--- app/src/main/java/com/example/listadecompras/CadastroActivity.kt
+++ app/src/main/java/com/example/listadecompras/CadastroActivity.kt
@@ -12,14 +12,32 @@ class CadastroActivity : AppCompatActivity() {
 
         btn_inserir.setOnClickListener(){
             val produto = et_nome_prod.text.toString()
+            val qtd = et_qtd_prod.text.toString()
+            val valor = et_valor_prod.text.toString()
 
-            if(produto.isNotEmpty()){
+            if(produto.isNotEmpty() && qtd.isNotEmpty() && valor.isNotEmpty()){
                 //adiciona item na lista
 
                 //limpa a caixa de texto
                 et_nome_prod.text.clear()
             } else {
-                et_nome_prod.error = "Preencha um valor"
+                if(et_nome_prod.text.isEmpty()){
+                    et_nome_prod.error = "Preencha o nome do produto"
+                } else {
+                    et_nome_prod.error = null
+                }
+
+                if(et_qtd_prod.text.isEmpty()){
+                    et_qtd_prod.error = "Preencha a quantidade do produto"
+                } else {
+                    et_qtd_prod.error = null
+                }
+
+                if(et_valor_prod.text.isEmpty()){
+                    et_valor_prod.error = "Preencha o valor do produto"
+                } else {
+                    et_valor_prod.error = null
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/listadecompras/Utils.kt
@@ -0,0 +1,4 @@
+package com.example.listadecompras
+
+//uma lista mutável do tipo Produto
+val produtosGlobal = mutableListOf<Produto>()
