125e5fe06892c3db01216a289f14fbd2fc630dcb
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-23 13:24:24 -0300

    Ajuste no carregamento da lista do adapter

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.listadecompras
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,23 +20,42 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-//        //criado o adapter para exibir os dados na lista
-//        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
-//
-//        //definindo o adaptador na lista
-//        lv_lista_produtos.adapter = produtosAdapter
-//
-//        lv_lista_produtos.setOnItemLongClickListener{
-//            adapterView, view, i, l ->
-//            //buscando o item clicado
-//            val item = produtosAdapter.getItem(i)
-//
-//            //removendo o item clicado da lista
-//            produtosAdapter.remove(item)
-//
-//            //retorno indicando que o click foi realizado com sucesso
-//            true
-//        }
+
+
+        //Implementação do adaptador
+        val produtosAdapter = ProdutoAdapter(this)
+
+        produtosAdapter.addAll(produtosGlobal)
+
+        //definindo o adaptador na lista
+        lv_lista_produtos.adapter = produtosAdapter
+
+        lv_lista_produtos.setOnItemLongClickListener{
+            adapterView, view, i, l ->
+            //buscando o item clicado
+            val item = produtosAdapter.getItem(i)
+
+            //removendo o item clicado da lista
+            produtosAdapter.remove(item)
+
+            //retorno indicando que o click foi realizado com sucesso
+            true
+        }
+
+
+    }
+
+    /**
+     * onResume é chamado quando a tela vai aparecer independentemente se está voltando ou criando
+     */
+    override fun onResume(){
+        super.onResume()
+
+        val adapter = lv_lista_produtos.adapter as ProdutoAdapter
+
+        adapter.clear()
+        adapter.addAll(produtosGlobal)
+
 
 
     }
